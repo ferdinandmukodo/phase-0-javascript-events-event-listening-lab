6bc9396cee46adc585441ea285436496bef4f786
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-08-23 15:44:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-function addingEventListener() {
-
-  const input = document.getElementById("input")
-  alert("I was clicked!")
-
-
-
+const input = document.getElementById('button')
+input.addEventListener('click', clickAlert)
+function addingEventListener(){
+    function clickAlert(){
+        alert("I was clicked")
+    }
+    const input = document.getElementById('button')
+    input.addEventListener('click', clickAlert)
+    // function clickAlert(){
+    //     alert("I was clicked")  
+    
+   
 }
-
-// const input = document.getElementById('input');
-
-// function clickAlert() {
-//   alert('I was clicked!');
-// }
-
-input.addEventListener('click', clickAlert);
-function
